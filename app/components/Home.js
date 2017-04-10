672eb336e15c6485cdea8e21c18ba30589f31a59
dates <jake.brooks@midsummerish.com>
2017-04-10 14:20:47 -0400

    funsies

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,16 +2,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './Home.css';
-
+import { Header, Button } from 'semantic-ui-react'
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
-          <Link to="/queryPage">to Query</Link>
+          <Header as='h1' size='huge'>Home</Header>
+          <Button.Group>
+          <Link to="/counter">
+            <Button>
+            Counter
+            </Button>
+          </Link>
+          <Link to="/queryPage">
+            <Button>
+            Query Page
+            </Button>
+          </Link>
+        </Button.Group>
         </div>
       </div>
     );
--- app/components/queryPage.js
+++ app/components/queryPage.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 import styles from './QueryPage.css';
-import { List } from 'semantic-ui-react'
+import { List, Button, Icon } from 'semantic-ui-react'
 import { BarChart, Bar, CartesianGrid, YAxis, XAxis, Tooltip } from 'recharts';
 import _ from 'lodash';
 class Counter extends Component {
@@ -14,17 +14,14 @@ class Counter extends Component {
     counter: number
   };
 
+  componentDidMount() {
+    this.props.queryOsquery('select * from "wifi_survey";')
+  }
+
   render() {
     const { queryOsquery } = this.props;
-    const data = [
-      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-    ];
+
+
     let response = _.sortBy(_.uniqBy(this.props.response, 'network_name'), ['rssi']);
     _.each(response, (item) => { item.rssi = (parseInt(item.rssi)*-1); return item;});
     console.log(response);
@@ -33,11 +30,11 @@ class Counter extends Component {
     );
     return (
       <div>
-          <div className={styles.backButton} data-tid="backButton">
-            <Link to="/">
-              <i className="fa fa-arrow-left fa-3x" />
-            </Link>
-          </div>
+        <Button icon>
+          <Link to="/">
+            <Icon name='arrow left' style={{color:"black"}} />
+          </Link>
+        </Button>
           <div  className={`query ${styles.query}`}>
               <h2>asdf</h2>
               <BarChart width={600} height={400} data={response}>
